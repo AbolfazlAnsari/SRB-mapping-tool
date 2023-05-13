1bcf8711a017e7b1a9ce291d4561d0fd070ab1fd
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-13 18:06:57 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,3 +1,16 @@
 helpers.installPackages("leaflet","maps","dplyr","shiny", "sf","shinydashboard","ggplot2")
 
 
+install.packages("packages/dplyr_1.0.7.tar.gz", repos=NULL, type="source")
+
+install.packages("packages/sp_1.0.7.tar.gz", repos=NULL, type="source")
+
+install.packages("packages/raster_1.0.7.tar.gz", repos=NULL, type="source")
+
+install.packages("packages/maps_1.0.7.tar.gz", repos=NULL, type="source")
+
+install.packages("packages/shiny_1.0.7.tar.gz", repos=NULL, type="source")
+
+install.packages("packages/shinydashboard_1.0.7.tar.gz", repos=NULL, type="source")
+
+
