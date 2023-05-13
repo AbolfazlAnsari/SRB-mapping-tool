d195a108355d21214e592d8b7972f93b2ebf2edb
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-13 15:09:08 -0400

    Update server.R

--- server.R
+++ server.R
@@ -9,18 +9,7 @@ server<-function(input,output){
       
       setView(lng=-77.86,lat=40.79,zoom=6) %>%
       
-      addProviderTiles('Esri.WorldImagery') # %>%
-      
-      # addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G", attribution = 'Google')
-      
-      # addTiles()
-      
-     # addPolygons(data=SRB,color='black')%>%
-      
-     # addPolygons(data=Counties,color='black',weight = 1)%>%
-      
-     # addMarkers(lng=-77.86,lat=40.79,options = markerOptions(draggable = TRUE))
-    
+      addProviderTiles('Esri.WorldImagery') 
     
     
   })
