3f6ad31141ae8b501c0f7464a5dde21106ee383f
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-13 17:32:03 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,17 +1,3 @@
-my_packages<-c('leaflet','maps','dplyr','shiny', 'sf','shinydashboard','ggplot2')
-
-install_if_missing<-function(p){
-if(p %in% rownames(installed.packages())==FALSE){install.packages(p,repos='http://cran.us.r-project.org', dependencies=TRUE)}
-
-}
-
-invisible(sapply(my_packages, install_if_missing))
+helpers.installPackages("leaflet","maps","dplyr","shiny", "sf","shinydashboard","ggplot2")
 
 
-library(leaflet)
-library(maps)
-library(dplyr)
-library(shiny)
-library(sf)
-library(shinydashboard)
-library(ggplot2)
