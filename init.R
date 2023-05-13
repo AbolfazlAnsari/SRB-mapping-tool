6ae791f460b5d08c908aeaa5e2cb22d8f3a37837
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-13 18:13:13 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,16 +1,25 @@
-helpers.installPackages("leaflet","maps","dplyr","shiny", "sf","shinydashboard","ggplot2")
+#helpers.installPackages("leaflet","maps","dplyr","shiny", "sf","shinydashboard","ggplot2")
 
 
 install.packages("packages/dplyr_1.0.7.tar.gz", repos=NULL, type="source")
 
-install.packages("packages/sp_1.0.7.tar.gz", repos=NULL, type="source")
+install.packages("packages/sp_1.6-0.tar.gz", repos=NULL, type="source")
 
 install.packages("packages/raster_1.0.7.tar.gz", repos=NULL, type="source")
 
-install.packages("packages/maps_1.0.7.tar.gz", repos=NULL, type="source")
+install.packages("packages/leaflet_2.1.2.tar.gz", repos=NULL, type="source")
 
-install.packages("packages/shiny_1.0.7.tar.gz", repos=NULL, type="source")
+install.packages("packages/maps_3.4.1.tar.gz", repos=NULL, type="source")
 
-install.packages("packages/shinydashboard_1.0.7.tar.gz", repos=NULL, type="source")
+install.packages("packages/shiny_1.7.1.tar.gz", repos=NULL, type="source")
 
+install.packages("packages/shinydashboard_0.7.2.tar.gz", repos=NULL, type="source")
+
+library(dplyr)
+library(sp)
+library(raster)
+library(leaflet)
+library(maps)
+library(shiny)
+library(shinydashboard)
 
