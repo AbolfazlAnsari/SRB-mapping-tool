11dafe7cebfceb0da27e69422e80ff4f80858eac
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-13 13:42:12 -0400

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,27 @@
+ui <- dashboardPage(
+  
+  dashboardHeader(title = "Susquehanna River Basin Interactive Mapping Tool"),
+  
+  dashboardSidebar(
+    
+    selectInput("variable", label = "Select:", choices = c("Water", "Flood events", "Organic N", "Organic P",
+                                                              
+                                                              "NO3","NO2","NH4","Total N","Mineral P","Total P", "Sediment"  ))
+    
+    
+  ),
+  
+  dashboardBody(
+    
+              leafletOutput(outputId = "map"),
+                
+               textOutput("text"),
+                
+              box(width = 6, height =4,   plotOutput(outputId = "local")),
+                
+               box(width =6,height= 4, plotOutput(outputId = "global")) 
+                
+  )
+  
+  
+)
