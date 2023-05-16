d129efc44b4c36f2522dc6b69213edc4af3d9f3e
Rashid Ansari <54929681+AbolfazlAnsari@users.noreply.github.com>
2023-05-15 20:19:30 -0400

    ui.R

--- ui.R
+++ ui.R
@@ -1,28 +1,100 @@
+ui <- dashboardPage(
+  
+  dashboardHeader(title ='Susquehanna River'),            
+                  
+  
+  dashboardSidebar( 
+    
+    
+    
+    sidebarMenu(id="menu",
+    
+    menuItem("Control",
+             
+             
+             menuSubItem('Map'),
+    
+    
+    
+    checkboxInput("Levee", label="Levee systems", value = FALSE, width = NULL),
+    
+    checkboxInput("Reservoir", label="Reservoirs", value = FALSE, width = NULL),
+    
+    
+    menuSubItem('Plot'),
+    
+    selectInput(inputId="variable", label = "Select:",selected ="Organic N" , choices = c("Water",'Flood event', "Organic N", "Organic P",
+                                                           
+      
+                                                           
+                                              "NO3","NO2","NH4","Mineral P", 'Total N','Total P', "Sediment"))
+    
+    ),
+    
+    
+    menuItem("Scenario",
+    
+             menuSubItem('Climate Change'),
+             
+             menuSubItem('Developments'),
+             
+             menuSubItem('BMPs')
+    
+    ),
+    
+    
+    
+    menuItem("Detailed Analysis"),
+    
+    menuItem("Raw Data"),
+    
+    menuItem("Downloads"),
+    
+    menuItem("About Project"),
+    
+    menuItem("Contact Us")
+    
 
-ui <- shinydashboard::dashboardPage(
-  
-  dashboardHeader(title = "Susquehanna River Basin Interactive Mapping Tool"),
-  
-  shinydashboard::dashboardSidebar(
     
-    selectInput("variable", label = "Select:", choices = c("Water", "Flood events", "Organic N", "Organic P",
-                                                              
-                                                              "NO3","NO2","NH4","Total N","Mineral P","Total P", "Sediment"  ))
-    
-    
-  ),
-  
-  shinydashboard::dashboardBody(
-    
-              leafletOutput(outputId = "map"),
-                
-               textOutput("text"),
-                
-              box(width = 6, height =4,   plotOutput(outputId = "local")),
-                
-               box(width =6,height= 4, plotOutput(outputId = "global")) 
-                
   )
   
   
+  
+  
+),
+
+
+
+  
+  dashboardBody(
+    
+  
+
+    
+    fluidRow( 
+      
+      column( width = 12, h2('Map'),h4('Click a location and drag the marker around'),  leafletOutput(outputId = "map"), textOutput("text")),
+      
+      
+      
+    ),
+    
+              
+      fluidRow(
+        
+        column(width=6,h2('Catchment load'),h4(textOutput("information1")),plotOutput(outputId = "local")),
+        
+         column(width=6,h2('Upstream + catchment load'),h4(textOutput("information2")),plotOutput(outputId = "global") )          
+              
+              
+      )            
+
+  
+
+  
 )
+
+) # end of user interface
+
+
+
